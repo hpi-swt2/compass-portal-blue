991f10312d51bd6452d8e4964df427c19b2a340b
Niklas Mohrin <dev@niklasmohrin.de>
2021-11-23 16:26:09 +0100

    Use OpenStreetMap as a provider

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,4 +1,4 @@
-Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/YOUR-CLOUDMADE-API-KEY/997/256/{z}/{x}/{y}.png"
-# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
-Leaflet.attribution = "Your attribution statement"
+Leaflet.tile_layer = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+Leaflet.subdomains = ["a", "b", "c"]
+Leaflet.attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 Leaflet.max_zoom = 18
