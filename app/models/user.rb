e4d3416440621a046c3447da904014355a39e032
Benedikt Weber <benedikt.web23@gmail.com>
2021-11-25 17:33:06 +0100

    Format using prettier

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[openid_connect]
-  
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :omniauthable,
+         omniauth_providers: %i[openid_connect]
+
   validates :phone_number, phone: true, allow_blank: true
   before_save :normalize_phone_number
   has_one_attached :profile_picture
@@ -17,26 +21,33 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       # All information returned by OpenID Connect is passed in `auth` param
       user.email = auth.info.email
+
       # Generate random password, default length is 20
       # https://www.rubydoc.info/github/plataformatec/devise/Devise.friendly_token
       user.password = Devise.friendly_token[0, 20]
       user.username = auth.info.name
+
       # `first_name` & `last_name` are also available
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
+
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
-      user.profile_picture.attach(io: File.open('app/assets/images/default-profile-picture.png'), filename: 'default-profile-picture.png', content_type: 'image/png')
+      user.profile_picture.attach(
+        io: File.open('app/assets/images/default-profile-picture.png'),
+        filename: 'default-profile-picture.png',
+        content_type: 'image/png'
+      )
     end
   end
-  
+
   def formatted_phone_number
     parsed_phone = Phonelib.parse(phone_number)
     return phone_number if parsed_phone.invalid?
-    parsed_phone.full_international 
+    parsed_phone.full_international
   end
-  
+
   # https://github.com/heartcombo/devise/wiki/OmniAuth:-Overview
   # Implement the following if you want to enable copying over data from an
   # OmniAuth provider after a user alsready has a session, i.e. is already logged in
@@ -47,7 +58,7 @@ class User < ApplicationRecord
   #     end
   #   end
   # end
-  
+
   private
 
   def normalize_phone_number
