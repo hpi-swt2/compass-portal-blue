8c79d01ee34d494dc871f2ce016dbe244b1356dd
Max Hoffmann <hoffmann7@uni-potsdam.de>
2021-11-30 10:09:13 +0100

    fixed default value problem for foreign key

--- db/migrate/20211129160951_add_buildings_to_rooms.rb
+++ db/migrate/20211129160951_add_buildings_to_rooms.rb
@@ -1,5 +1,5 @@
 class AddBuildingsToRooms < ActiveRecord::Migration[6.1]
   def change
-    add_reference :rooms, :building, null: false, foreign_key: true
+    add_reference :rooms, :building, default: nil, foreign_key: true
   end
 end
