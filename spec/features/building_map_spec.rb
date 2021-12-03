e2d1fb001ca921699bb38148520dff9709693902
JulianEgbert <Julian.Egbert@student.hpi.de>
2021-12-03 11:09:11 +0100

    Add letters to map and tests

--- spec/features/building_map_spec.rb
+++ spec/features/building_map_spec.rb
@@ -15,7 +15,14 @@ describe "Building Map page", type: :feature do
   it "highlights builings on the map", js: true do
     visit building_map_path
     expect(page).to have_css(".leaflet-interactive")
-    expect(page).to have_selector("path.leaflet-interactive", count: 15)
+    expect(page).to have_selector("path.leaflet-interactive", minimum: 15)
+  end
+
+  it "shows the name of the HPI buildings", js: true do
+    visit building_map_path
+    expect(page).to have_css(".leaflet-marker-pane")
+    expect(page).to have_css(".leaflet-marker-icon")
+    expect(page).to have_selector("div.leaflet-marker-icon", minimum: 13)
   end
 
   after do
