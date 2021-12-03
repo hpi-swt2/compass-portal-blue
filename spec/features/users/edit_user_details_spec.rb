dfd35de0a3f274ce46a8f37de63681da0d743912
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-12-03 12:35:38 +0100

    Add profile picture view and file input on edit user details page
    Co-authored-by: TheGrayStone <lissner.nils@liwe-design.com>

--- spec/features/users/edit_user_details_spec.rb
+++ spec/features/users/edit_user_details_spec.rb
@@ -1,18 +1,52 @@
 require "rails_helper"
 
-RSpec.describe "User details", type: :feature do
-  let(:user) { FactoryBot.create(:user) }
+RSpec.describe "User details page", type: :feature do
+  before :each do
+    @user = FactoryBot.create(:user)
+    @user.profile_picture.attach(
+      io: File.open('app/assets/images/default-profile-picture.png'),
+      filename: 'default-profile-picture.png',
+      content_type: 'image/png'
+    )
+  end
 
-  it "are not viewable when not logged in" do
+  it "is not viewable when not logged in" do
     visit edit_user_registration_path
     expect(page).to have_css('.alert-danger')
     expect(page).not_to have_current_path(edit_user_registration_path)
   end
 
-  it "are viewable after login" do
-    sign_in user
+  it "is viewable after login" do
+    sign_in @user
     visit edit_user_registration_path
     expect(page).not_to have_css('.alert-danger')
     expect(page).to have_current_path(edit_user_registration_path)
+  end
+
+  it "includes profile picture input" do
+    sign_in @user
+    visit edit_user_registration_path
+    expect(page).to have_field 'user[profile_picture]'
+  end
+
+  it "includes profile picture view" do
+    sign_in @user
+    visit edit_user_registration_path
+    expect(page).to have_css "img[alt='#{@user.profile_picture.filename}']"
+  end
+
+  it "can update profile picture" do
+    filename = @user.profile_picture.filename
+    @user.profile_picture.detach
+
+    sign_in @user
+    visit edit_user_registration_path
+    expect(page).to_not have_css "img[alt='#{filename}']"
+
+    page.attach_file('user[profile_picture]', 'app/assets/images/default-profile-picture.png')
+    page.find('input[type=submit][name=commit]').click
+
+    visit edit_user_registration_path
+    expect(page).to have_css "img[alt='#{filename}']"
   end
 end
