78a3487dab8dde917dade5f4c69acfe6d436a9b4
Olliwehr <oliver.hess@student.hpi.uni-potsdam.de>
2021-12-06 15:40:39 +0100

    Fix issues introduced while merging changes from dev

--- spec/features/users/edit_user_details_spec.rb
+++ spec/features/users/edit_user_details_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "User details page", type: :feature do
     expect(page).not_to have_css('.alert-danger')
     expect(page).to have_current_path(edit_user_registration_path)
   end
-  
+
   it "includes profile picture input" do
     sign_in @user
     visit edit_user_registration_path
@@ -51,38 +51,38 @@ RSpec.describe "User details page", type: :feature do
   end
 
   it "includes an input field to change the username which displays the current username" do
-    sign_in user
+    sign_in @user
     visit edit_user_registration_path
-    expect(page).to have_field('user[username]', with: user.username)
+    expect(page).to have_field('user[username]', with: @user.username)
   end
 
   it "includes an input field to change the first name" do
-    sign_in user
+    sign_in @user
     visit edit_user_registration_path
     expect(page).to have_field('user[first_name]')
   end
 
   it "includes an input field to change the last name" do
-    sign_in user
+    sign_in @user
     visit edit_user_registration_path
     expect(page).to have_field('user[last_name]')
   end
 
   it "includes an input field to change the email address" do
-    sign_in user
+    sign_in @user
     visit edit_user_registration_path
     expect(page).to have_field('user[email]')
   end
 
   it "includes an input field to change the phone number" do
-    sign_in user
+    sign_in @user
     visit edit_user_registration_path
     expect(page).to have_field('user[phone_number]')
   end
 
   it "includes an input field to change the room where the person can be found" do
     @room = create :room
-    sign_in user
+    sign_in @user
     visit edit_user_registration_path
     expect(page).to have_select('user[rooms][]', with_options: [@room.name])
   end
