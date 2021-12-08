d21e9040fe15e93c0abe151ffb52c1053ceeed8a
rdunker <bm.rebekka@googlemail.com>
2021-12-08 19:37:20 +0100

    Switch longitude and altitude.

--- app/helpers/routing_helper.rb
+++ app/helpers/routing_helper.rb
@@ -3,12 +3,15 @@ require 'json'
 
 module RoutingHelper
   def self.routes
-    url = URI.parse('http://router.project-osrm.org/route/v1/driving/52.383353,13.094673;52.397339,13.135604?overview=full&geometries=geojson')
+    url = URI.parse('http://router.project-osrm.org/route/v1/foot/13.133082,52.393913;13.129606,52.393861?overview=full&geometries=geojson')
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
     }
     json_response = JSON.parse(res.body)
-    [{ latlngs: json_response["routes"][0]["geometry"]["coordinates"] }]
+    coordinates = json_response["routes"][0]["geometry"]["coordinates"].map do |coordinate|
+      [coordinate.second, coordinate.first]
+    end
+    [{ latlngs: coordinates }]
   end
 end
