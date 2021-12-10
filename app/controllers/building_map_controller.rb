8702f95c65e0b130d3901fdf93060fb93776d74f
Finn-HPI <finn.schoellkopf@student.hpi.uni-potsdam.de>
2021-12-10 11:15:57 +0100

    refactor code based on review

--- app/controllers/building_map_controller.rb
+++ app/controllers/building_map_controller.rb
@@ -1,7 +1,7 @@
 class BuildingMapController < ApplicationController
   def index
-    @start = params[:start]
-    @destination = params[:dest]
+    @start = RoutingHelper.valid_coordinates params[:start] ? params[:start]: nil
+    @destination = RoutingHelper.valid_coordinates params[:dest] ? params[:dest]: nil
     @route = RoutingHelper.calculate_route(@start, @destination)
   end
 end
--- app/helpers/routing_helper.rb
+++ app/helpers/routing_helper.rb
@@ -2,25 +2,29 @@ require 'httparty'
 require 'json'
 
 module RoutingHelper
-  def self.seconds_to_minsec(sec)
+  def self.format_seconds_as_minsec(sec)
     format("%<minutes>.2d:%<seconds>.2d", minutes: sec / 60, seconds: sec % 60)
   end
 
   def self.valid_coordinates(coordinates)
     return false if coordinates.blank?
 
-    regex = /^[0-9]{1,2}(\.[0-9]{1,6})?,[0-9]{1,2}(\.[0-9]{1,6})?$/
+    regex = /^-?\d{1,2}(\.\d{1,6})?,-?\d{1,2}(\.\d{1,6})?$/
     coordinates.match(regex)
+  end
+
+  def self.routing_url(start, destination)
+    start_location = start.split(",")
+    dest_location = destination.split(",")
+    # The API requires long, lat instead of lat, long so we have to swap those
+    "http://routing.openstreetmap.de/routed-foot/route/v1/driving/#{start_location[1]},#{start_location[0]};#{dest_location[1]},#{dest_location[0]}?overview=full&geometries=geojson"
   end
 
   def self.calculate_route(start, destination)
     return nil unless valid_coordinates(start) && valid_coordinates(destination) # No route requested
 
-    start_location = start.split(",")
-    dest_location = destination.split(",")
-    # The API requires long, lat instead of lat, long so we have to swap those
     begin
-      response = HTTParty.get("http://routing.openstreetmap.de/routed-foot/route/v1/driving/#{start_location[1]},#{start_location[0]};#{dest_location[1]},#{dest_location[0]}?overview=full&geometries=geojson")
+      response = HTTParty.get(routing_url(start, destination))
       return nil unless response.code == 200
 
       JSON.parse(response.body)["routes"][0]
@@ -32,7 +36,7 @@ module RoutingHelper
   def self.transform_route_to_time_marker(route)
     return [] unless route
 
-    walking_time = seconds_to_minsec(route["duration"])
+    walking_time = format_seconds_as_minsec(route["duration"])
     start = route["geometry"]["coordinates"][0]
     [{
       latlng: [start.second, start.first],
@@ -49,6 +53,6 @@ module RoutingHelper
     coordinates = route["geometry"]["coordinates"].map do |coordinate|
       [coordinate.second, coordinate.first]
     end
-    [{ latlngs: coordinates, options: { className: "routing-path" } }]
+    { latlngs: coordinates, options: { className: "routing-path" } }
   end
 end
