d1db13626fd4a77f1660d16537a8f5cd8eb1c29c
Benedikt Vidic <benedikt.vidic@gmx.de>
2021-12-10 17:26:59 +0100

    Fixed delete openingtimes and added the option to add an openingtime in the edit person page

--- spec/models/openingtime_spec.rb
+++ spec/models/openingtime_spec.rb
@@ -10,24 +10,24 @@ RSpec.describe Openingtime, type: :model do
     expect(@openingtime.day).to eq(1)
   end
 
-  it "should accept days between 1 and 7" do
-    @openingtime.day = 1
+  it "should accept days between 0 and 6" do
+    @openingtime.day = 0
     expect(@openingtime).to be_valid
     @openingtime.day = 4
     expect(@openingtime).to be_valid
-    @openingtime.day = 7
+    @openingtime.day = 6
     expect(@openingtime).to be_valid
   end
 
-  it "should reject days lower than 1" do
-    @openingtime.day = 0
+  it "should reject days lower than 0" do
+    @openingtime.day = -1
     expect(@openingtime).to be_invalid
     @openingtime.day = -5
     expect(@openingtime).to be_invalid
   end
 
-  it "should reject days larger than 7" do
-    @openingtime.day = 8
+  it "should reject days larger than 6" do
+    @openingtime.day = 7
     expect(@openingtime).to be_invalid
     @openingtime.day = 1000
     expect(@openingtime).to be_invalid
