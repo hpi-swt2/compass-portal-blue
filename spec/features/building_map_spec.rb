ab280794ea7a1fe0b17604931a674abb59e5bd08
TheGrayStone <nilslissner@gmx.de>
2021-12-10 12:43:58 +0100

    Add tests for new UI layout
    Co-authored-by: philbow61 <philbow61@users.noreply.github.com>

--- spec/features/building_map_spec.rb
+++ spec/features/building_map_spec.rb
@@ -64,4 +64,10 @@ describe "Building Map page", type: :feature do
     expect(page).to have_css('.target-pin')
   end
 
+  it "indicates wich view is enabled" do
+    visit building_map_path
+    expect(page).to have_css('.nav-link.active i.fa-map')
+    expect(page).to have_css('.nav-link:not(.active) i.fa-search')
+  end
+
 end
