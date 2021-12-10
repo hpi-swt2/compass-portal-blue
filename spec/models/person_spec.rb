152ce19507033a6d9d5e4e2197ef8fb82176553e
Magnus Menger <magnus@magnusmenger.de>
2021-12-10 15:23:23 +0100

    Added tests for user person link

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Person, type: :model do
     expect(person.phone_number).to eq("+4990909090")
     expect(person.email).to eq("herbert.herbertson@hpi.de")
   end
+
+  it "can be created from omniauth" do
+    Info = Struct.new(:first_name, :last_name, :email)
+    Auth = Struct.new(:info)
+    info = Info.new("Herbert", "Herbertson", "herbert.herbertson@hpi.de")
+    auth = Auth.new(info)
+    person = Person.from_omniauth(auth)
+    expect(person.first_name).to eq("Herbert")
+    expect(person.last_name).to eq("Herbertson")
+    expect(person.email).to eq("herbert.herbertson@hpi.de")
+  end
 end
