b7ad52ea9306b22e179155570581a09e15efa372
TheGrayStone <nilslissner@gmx.de>
2021-12-10 12:44:18 +0100

    Remove protected page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,9 @@
 class WelcomeController < ApplicationController
-  # https://github.com/heartcombo/devise#controller-filters-and-helpers
-  before_action :authenticate_user!, only: [:protected]
-  # Alternative: before_action :authenticate_user!, :except => [:index]
-
   def index
     # Welcome page, accessible without login
   end
 
-  def protected
+  def login
     # Only accessible by logged in users, see `before_action` call
   end
 end
