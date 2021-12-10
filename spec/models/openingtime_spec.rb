3c3aeb95e9cd1fd936f708fa45a0ba5a842df447
Benedikt Vidic <benedikt.vidic@gmx.de>
2021-12-10 10:37:03 +0100

    Added openingtimes-day validation tests

--- spec/models/openingtime_spec.rb
+++ spec/models/openingtime_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Openingtime, type: :model do
+
+  before(:each) do
+    @openingtime = FactoryBot.create(:openingtime)
+  end
+
   it "does not fail when created with factory bot" do
-    openingtime = FactoryBot.create(:openingtime)
-    expect(openingtime.day).to eq(1)
+    expect(@openingtime.day).to eq(1)
+  end
+
+  it "should accept days between 1 and 7" do
+    @openingtime.day = 1
+    expect(@openingtime).to be_valid
+    @openingtime.day = 4
+    expect(@openingtime).to be_valid
+    @openingtime.day = 7
+    expect(@openingtime).to be_valid
+  end
+
+  it "should reject days lower than 1" do
+    @openingtime.day = 0
+    expect(@openingtime).to be_invalid
+    @openingtime.day = -5
+    expect(@openingtime).to be_invalid
+  end
+
+  it "should reject days larger than 7" do
+    @openingtime.day = 8
+    expect(@openingtime).to be_invalid
+    @openingtime.day = 1000
+    expect(@openingtime).to be_invalid
   end
 end
