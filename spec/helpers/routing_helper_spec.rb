fa09100170a6d6a1827c8bae160072787b690bef
christian-helms <christian.z.helms@gmail.com>
2021-12-13 20:29:40 +0100

    add tests for coordinate lookup

--- spec/helpers/routing_helper_spec.rb
+++ spec/helpers/routing_helper_spec.rb
@@ -46,4 +46,24 @@ describe "Routing helper", type: :helper do
     expect(RoutingHelper.format_seconds_as_minsec(514)).to eq "08:34"
     expect(RoutingHelper.format_seconds_as_minsec(30)).to eq "00:30"
   end
+
+  context "coordinate lookup" do
+    it "lets valid coordinates pass" do
+      location = "52.393913,13.133082"
+      expect(RoutingHelper.coordinates(location, "")).to eq location
+    end
+
+    it "returns your location if requested" do
+      location = "52.393913,13.133082"
+      expect(RoutingHelper.coordinates("Your location", location)).to eq location
+    end
+
+    it "looks up the locations of buildings" do
+      expect(RoutingHelper.coordinates("Haus G", "")).to eq "52.3947488,13.1248368"
+    end
+
+    it "rejects invalid input" do
+      expect(RoutingHelper.coordinates("Haus ASDFQWERTZ", "")).to eq nil
+    end
+  end
 end
