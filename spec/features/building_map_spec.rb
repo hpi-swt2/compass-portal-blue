4e6a3ef7405e752cc1632b77a6f019b41d4e8043
TheGrayStone <nilslissner@gmx.de>
2021-12-13 11:50:15 +0100

    Fix linting 3: The lintening

--- spec/features/building_map_spec.rb
+++ spec/features/building_map_spec.rb
@@ -12,37 +12,37 @@ describe "Building Map page", type: :feature do
 
   describe "map" do
     before do
-        Capybara.current_driver = :selenium_chrome_headless
-        Capybara.ignore_hidden_elements = false
+      Capybara.current_driver = :selenium_chrome_headless
+      Capybara.ignore_hidden_elements = false
     end
 
     after do
-        Capybara.current_driver = :default
+      Capybara.current_driver = :default
     end
 
     it "contains a map", js: true do
-        visit building_map_path
-        expect(page).to have_css("#map")
-        expect(page).to have_css(".leaflet-container")
+      visit building_map_path
+      expect(page).to have_css("#map")
+      expect(page).to have_css(".leaflet-container")
     end
 
     it "highlights builings on the map", js: true do
-        visit building_map_path
-        expect(page).to have_css(".leaflet-interactive")
-        expect(page).to have_selector("path.building", count: 15)
+      visit building_map_path
+      expect(page).to have_css(".leaflet-interactive")
+      expect(page).to have_selector("path.building", count: 15)
     end
 
     it "separates HPI and Uni-Potsdam buildings" do
-        visit building_map_path
-        expect(page).to have_selector("path.hpi-building", count: 13)
-        expect(page).to have_selector("path.uni-potsdam-building", count: 2)
+      visit building_map_path
+      expect(page).to have_selector("path.hpi-building", count: 13)
+      expect(page).to have_selector("path.uni-potsdam-building", count: 2)
     end
 
     it "shows the name of the HPI buildings", js: true do
-        visit building_map_path
-        expect(page).to have_css(".leaflet-marker-pane")
-        expect(page).to have_css(".leaflet-marker-icon")
-        expect(page).to have_selector("div.building-icon", minimum: 13)
+      visit building_map_path
+      expect(page).to have_css(".leaflet-marker-pane")
+      expect(page).to have_css(".leaflet-marker-icon")
+      expect(page).to have_selector("div.building-icon", minimum: 13)
     end
 
     it "shows the pin of a target point", js: true do
@@ -51,21 +51,21 @@ describe "Building Map page", type: :feature do
     end
 
     context "with route" do
-        it "shows a calculated route", js: true do
+      it "shows a calculated route", js: true do
         visit building_map_path(start: "52.393913,13.133082", dest: "52.393861,13.129606")
         expect(page).to have_css(".routing-path")
-        end
+      end
 
-        it "shows no route, if it's not requested", js: true do
+      it "shows no route, if it's not requested", js: true do
         visit building_map_path
         expect(page).not_to have_css(".routing-path")
         expect(page).not_to have_css(".time-icon")
-        end
+      end
 
-        it "shows the time of a calculated route", js: true do
+      it "shows the time of a calculated route", js: true do
         visit building_map_path(start: "52.393913,13.133082", dest: "52.393861,13.129606")
         expect(page).to have_css(".time-icon")
-        end
+      end
     end
   end
 end
