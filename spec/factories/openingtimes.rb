90bcb993acdabb4e050f1d6541fa8e7476770f59
sinahenning <henningsina@gmail.com>
2021-12-15 15:07:22 +0100

    added model tests for location

--- spec/factories/openingtimes.rb
+++ spec/factories/openingtimes.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     opens { Tod::TimeOfDay.new 8 }
     closes { Tod::TimeOfDay.new 12 }
     day { 1 }
-    association :timeable, factory: :building
+    association :timeable, factory: [:building, :location]
 
   end
 end
--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Building, type: :model do
-  it "has name, location_latitude, location_longitude" do
-    building = FactoryBot.create :building
+  building = FactoryBot.create :building
+  openingtime = FactoryBot.create(:openingtime, timeable: building)
 
+  it "has name, location_latitude, location_longitude" do
     expect(building.name).to eq('A')
     expect(building.location_latitude).to eq(1.5)
     expect(building.location_longitude).to eq(3.5)
+  end
+
+  it "has openingtime" do
+    expect(openingtime.timeable).to eq(building)
   end
 end
