1ffbe43f6225165b24c1a71e6c54ec4b3564caab
Flo-Mueller <florian.mueller141@web.de>
2021-12-16 12:16:59 +0100

    Implemented Feature and changed test to be in accordance to other homepage buttons

--- spec/views/welcome/index.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "welcome/index", type: :view do
-  it "has a room button" do
-    render
-    expect(rendered).to have_link "Rooms", href: "rooms/index"
-  end
-end
