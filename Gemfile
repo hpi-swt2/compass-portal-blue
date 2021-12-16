249777f561d7506a8731411b4fd5a8a7b11b22f9
Storyxx <57802448+Storyxx@users.noreply.github.com>
2021-12-16 13:59:27 +0100

    rename branch (#104)
    * Implement labels with dummy data.
    
    * Prove of concept - access to OWA calendar data
    
    * Get todays calendar items much easyer and quicker
    
    * Moved room occupancy extraction to room model
    
    Co-authored-by: Maximilian Franz <maximilian.franz@student.hpi.de>

--- Gemfile
+++ Gemfile
@@ -119,3 +119,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "phonelib"
+
+gem "viewpoint"
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,55 @@
+# The model representing a room at HPI
 class Room < ApplicationRecord
   belongs_to :building
   has_and_belongs_to_many :people
   validates :name, presence: true
+  validates :room_type, presence: true
   validates :floor, presence: true, numericality: { only_integer: true }
+
+  def todays_calendar_items(calendar)
+
+    # https://docs.microsoft.com/en-us/exchange/client-developer/web-service-reference/calendarview
+    # +
+    # https://github.com/WinRb/Viewpoint/blob/e8fec4ab1af25fc128062cd96770afdb9fc38c68/lib/ews/soap/builders/ews_builder.rb#L695
+    
+    calendar_view = {:calendar_view => {
+      :max_entries_returned => 100,
+      :start_date => DateTime.current.beginning_of_day,
+      :end_date => DateTime.current.end_of_day
+    }}
+
+    calendar.items(calendar_view)
+  end
+
+  def free?
+    endpoint = "https://owa.hpi.uni-potsdam.de/ews/Exchange.asmx"
+    file = File.open("tmp_credentials.txt")
+    file_data = file.readlines.map(&:chomp)
+    user = file_data[0] # "HPI\\firstname.lastname"
+    pass = file_data[1] # "password"
+
+    cli = Viewpoint::EWSClient.new endpoint, user, pass
+    # ---------------------------------------------------------------------- #
+
+    room_management_folder = cli.get_folder_by_name "HPI Raumverwaltung", parent: :publicfoldersroot
+
+    # TODO do this for all room types
+    room_room_type_folder = cli.get_folder_by_name room_type, parent: room_management_folder.id
+
+    # TODO do this for all rooms
+    room_calendar = cli.get_folder_by_name name, parent: room_room_type_folder.id
+
+    calendar_items = todays_calendar_items(room_calendar)
+
+    current_datetime = DateTime.now()
+
+    free = false
+    calendar_items.each do |item|
+      free = item.start > current_datetime || current_datetime < item.end
+      if !free
+        return free
+      end
+    end
+    return free
+  end
 end
