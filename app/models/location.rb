3d28f6bfc048bea9f7c9fe65e75bfb797417fdfb
Magnus Menger <magnus@magnusmenger.de>
2022-01-05 13:16:44 +0100

    added validations for locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,9 @@
 class Location < ApplicationRecord
   include Timeable
+
+  validates :name, :location_longitude, :location_latitude, presence: true
   
   has_one_attached :location_photo
-
   accepts_nested_attributes_for :openingtimes, allow_destroy: true
 
   def self.location_photo()  
