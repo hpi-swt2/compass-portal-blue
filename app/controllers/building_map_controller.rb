041785383e016c0623d3e483626aaeeae2f3becd
rdunker <bm.rebekka@googlemail.com>
2022-01-05 20:09:18 +0100

    Add marker approach.

--- app/controllers/building_map_controller.rb
+++ app/controllers/building_map_controller.rb
@@ -5,7 +5,6 @@ class BuildingMapController < ApplicationController
     @start = RoutingHelper.resolve_coordinates(params[:start])
     @destination = RoutingHelper.resolve_coordinates(params[:dest])
     @route = RoutingHelper.calculate_route(@start, @destination) if @start.present? && @destination.present?
-
     @target = params[:target]
   end
 end
