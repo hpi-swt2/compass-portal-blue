15b56c3c09add4dbd115b63d69465c36dbdb9248
Magnus Menger <magnus@magnusmenger.de>
2022-01-05 13:50:32 +0100

    fixed broken test

--- spec/features/locations/new_spec.rb
+++ spec/features/locations/new_spec.rb
@@ -14,17 +14,15 @@ describe "New location page", type: :feature do
     expect(page).to have_field('location[location_longitude]')
     #expect(page).to have_field('location[opens]')
   end
-  it "should fill in form and submit" do
-    visit new_location_path 
+
+  it "has a form which creates database entries after filling out" do
+    visit new_location_path
     page.fill_in 'location[name]', with: 'test_name'
     page.fill_in 'location[details]', with: 'test_details'
-    page.fill_in 'location[location_photo]', with: ''
     page.fill_in 'location[location_latitude]', with: 10.0
     page.fill_in 'location[location_longitude]', with: 10.0
     find('input[type="submit"]').click
-    context "should be found in the database" do
-      location.exists?(:name => 'test_name', :details => 'test_details', :location_latitude => 10.0, :location_longitude => 10.0)
-    end
+    expect(Location.where(name: 'test_name', details: 'test_details', location_latitude: 10.0, location_longitude: 10.0)).to exist
   end
 
   
