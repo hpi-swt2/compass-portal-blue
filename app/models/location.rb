a0de7b7e9280ce0e209fcbf4d4632c418004e927
Magnus Menger <magnus@magnusmenger.de>
2022-01-06 14:38:50 +0100

    removed unused method

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
+# the model representing a location of interest
 class Location < ApplicationRecord
   include Timeable
 
@@ -11,12 +12,4 @@ class Location < ApplicationRecord
 
   has_one_attached :location_photo
   accepts_nested_attributes_for :openingtimes, allow_destroy: true
-
-  def self.location_photo
-    location.location_photo.attach(
-      io: File.open('app/assets/images/default-location-photo.png'),
-      filename: 'default-location-photo.png',
-      content_type: 'image/png'
-    )
-  end
 end
