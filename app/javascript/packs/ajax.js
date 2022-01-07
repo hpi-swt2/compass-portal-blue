b14800f0741f158a20ca85225b660db439ee170f
TheGrayStone <nilslissner@gmx.de>
2022-01-07 15:50:10 +0100

    Improve navigation by using history pop and push state and remove edit buttons from detail pages

--- app/javascript/packs/ajax.js
+++ app/javascript/packs/ajax.js
@@ -1,16 +1,24 @@
-global.ajaxCall = (event, target, valuesToSubmit = "") => {
+global.ajaxCall = (
+  event,
+  target,
+  valuesToSubmit = "",
+  canGetBack = false,
+  pushToHistory = true
+) => {
   const applyAjaxWrap = () => {
     $("#browse-outlet a").each(function () {
       const link = $(this);
       const href = link.attr("href");
       link.attr("href", "#");
       link.on("click", (event) => {
-        ajaxCall(event, href, "");
+        ajaxCall(event, href, "", true);
       });
     });
   };
-
-  event.preventDefault();
+  event?.preventDefault();
+  $("#browse-outlet").html(
+    '<div class="loading"><i class="fas fa-compass"></i></div>'
+  );
   $.ajax({
     type: "GET",
     url: target,
@@ -21,7 +29,50 @@ global.ajaxCall = (event, target, valuesToSubmit = "") => {
       $("#browse-outlet").html(
         parsed.querySelector("#app-outlet-outer").innerHTML
       );
+      if (pushToHistory) {
+        history.pushState(
+          {
+            canGetBack,
+          },
+          null,
+          `/map${target}?${valuesToSubmit}`
+        );
+      }
       applyAjaxWrap();
+      if (canGetBack) {
+        $("#floating-back").addClass("visible");
+      } else {
+        $("#floating-back").removeClass("visible");
+      }
     },
   });
 };
+
+function navigateToLocation(pushToHistory = false) {
+  $("#_overlay").addClass("open");
+  $("#toggle-overlay").addClass("visible");
+  $("#toggle-overlay").addClass("open");
+  const path = location.pathname.slice(4);
+  ajaxCall(
+    null,
+    path,
+    location.search.slice(1),
+    history?.state?.canGetBack ?? false,
+    pushToHistory
+  );
+}
+
+window.addEventListener("popstate", function (event) {
+  event.preventDefault();
+  const path = location.pathname;
+  if (path.slice(0, 5) === "/map/") {
+    navigateToLocation();
+  }
+});
+
+window.addEventListener("load", function () {
+  const path = location.pathname;
+  if (path.slice(0, 5) === "/map/") {
+    navigateToLocation(true);
+  }
+});
