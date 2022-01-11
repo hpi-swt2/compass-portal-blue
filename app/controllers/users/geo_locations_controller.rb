a3b30cf5c2baeccbac018c62d35619d96bda30cc
Niklas Mohrin <dev@niklasmohrin.de>
2022-01-11 19:39:38 +0100

    `bundle exec rubocop -a`

--- app/controllers/users/geo_locations_controller.rb
+++ app/controllers/users/geo_locations_controller.rb
@@ -1,20 +1,22 @@
 require "date"
 
 class Users::GeoLocationsController < ApplicationController
+  # FIXME: this should be cleaned every now and then
+  # Maps from user ids to (location, timestamp) tuples
+  @@locations = Concurrent::Hash.new
 
-    # FIXME: this should be cleaned every now and then
-    # Maps from user ids to (location, timestamp) tuples
-    @@locations = Concurrent::Hash.new
+  def update_geo_location
+    return head :unauthorized unless user_signed_in?
 
-    def update_geo_location
-      return head :unauthorized unless user_signed_in?
-
-      # TODO: should we validate that only exactly those are keys? maybe we
-      # lock ourself out of passing metadata / force us to clean out csrf
-      # tokens etc. beforehand?
-      location = params.require(:location)
-      return render(status: :bad_request, plain: "Invalid coordinates") unless RoutingHelper.valid_coordinates?(location)
-
-      @@locations[current_user.id] = [location, DateTime.now]
+    # TODO: should we validate that only exactly those are keys? maybe we
+    # lock ourself out of passing metadata / force us to clean out csrf
+    # tokens etc. beforehand?
+    location = params.require(:location)
+    unless RoutingHelper.valid_coordinates?(location)
+      return render(status: :bad_request,
+                    plain: "Invalid coordinates")
     end
+
+    @@locations[current_user.id] = [location, DateTime.now]
+  end
 end
