06517dcac3847644e7d615633f20eb2c134743f0
JulianEgbert <Julian.Egbert@student.hpi.de>
2022-01-11 13:36:12 +0100

    remove wait_for_ajax

--- spec/features/building_map_spec.rb
+++ spec/features/building_map_spec.rb
@@ -1,14 +1,5 @@
 require "rails_helper"
 
-def wait_for_ajax
-  timer_end = Time.current + 5.seconds
-  while page.evaluate_script('$.active') != 0
-    raise "Page took more than 5 seconds to load via ajax" if Time.current > timer_end
-
-    sleep 0.1
-  end
-end
-
 describe "Building Map page", type: :feature do
 
   describe "layout" do
@@ -19,7 +10,7 @@ describe "Building Map page", type: :feature do
     end
   end
 
-  describe "map", inconsistent: true do
+  describe "map" do
     before do
       Capybara.current_driver = :selenium_chrome_headless
       Capybara.ignore_hidden_elements = false
@@ -31,14 +22,12 @@ describe "Building Map page", type: :feature do
 
     it "contains a map", js: true do
       visit building_map_path
-      wait_for_ajax
       expect(page).to have_css("#map")
       expect(page).to have_css(".leaflet-container")
     end
 
     it "highlights buildings on the map", js: true do
       visit building_map_path
-      wait_for_ajax
       page.assert_selector('path.building', count: 15, wait: 5)
       expect(page).to have_css(".leaflet-interactive")
       expect(page).to have_selector("path.building", count: 15)
@@ -46,7 +35,6 @@ describe "Building Map page", type: :feature do
 
     it "separates HPI and Uni-Potsdam buildings", js: true do
       visit building_map_path
-      wait_for_ajax
       page.assert_selector('path.hpi-building', minimum: 1, wait: 5)
       expect(page).to have_selector("path.hpi-building", count: 13)
       expect(page).to have_selector("path.uni-potsdam-building", count: 2)
@@ -54,7 +42,6 @@ describe "Building Map page", type: :feature do
 
     it "shows the name of the HPI buildings", js: true do
       visit building_map_path
-      wait_for_ajax
       page.assert_selector("div.building-icon", minimum: 13, wait: 5)
       expect(page).to have_css(".leaflet-marker-pane")
       expect(page).to have_css(".leaflet-marker-icon")
@@ -63,25 +50,21 @@ describe "Building Map page", type: :feature do
 
     it "shows the pin of a target point", js: true do
       visit building_map_path(target: "52.393913,13.133082")
-      wait_for_ajax
-      page.assert_selector(".target-pin", wait: 5)
+      find(".target-pin", wait: 5)
       expect(page).to have_css(".target-pin")
     end
 
     it "shows no route, if it's not requested", js: true do
       visit building_map_path
-      wait_for_ajax
       expect(page).not_to have_css(".routing-path")
       expect(page).not_to have_css(".time-icon")
     end
 
     it "renders the map with all special features", js: true do
       visit building_map_path
-      wait_for_ajax
       expect(page).to have_css("#map")
       expect(page).to have_css(".leaflet-container")
       page.assert_selector("path.building", count: 15, wait: 5)
-      expect(page).to have_selector("path.building", count: 15)
       expect(page).to have_selector("path.hpi-building", count: 13)
       expect(page).to have_selector("div.building-icon", minimum: 13)
       expect(page).to have_selector("path.uni-potsdam-building", count: 2)
@@ -95,21 +78,20 @@ describe "Building Map page", type: :feature do
         fill_in 'start', with: 'Haus A'
         fill_in 'dest', with: 'Haus L'
         click_on 'Go'
-        wait_for_ajax
       end
 
       it "shows a calculated route", js: true do
-        find(".routing-path", wait: 5)
+        find(".routing-path", wait: 15)
         expect(page).to have_css(".routing-path")
       end
 
       it "shows the time of a calculated route", js: true do
-        find(".time-icon", wait: 5)
+        find(".time-icon", wait: 15)
         expect(page).to have_css(".time-icon")
       end
 
       it "only shows one route at the time", js: true do
-        find(".routing-path", wait: 5)
+        find(".routing-path", wait: 15)
         expect(page).to have_css(".routing-path", count: 1)
         fill_in 'start', with: 'Haus A'
         fill_in 'dest', with: 'Haus L'
