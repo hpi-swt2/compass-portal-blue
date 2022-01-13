d6cd2d3bc35ad7a6ab91b488bab10d1c77d1fd66
Niklas Mohrin <dev@niklasmohrin.de>
2022-01-13 14:38:32 +0100

    Allow us to use `sign_in` in `:request` specs.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -85,4 +85,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # This allows us to use `sign_in` in `:request` specs.
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
