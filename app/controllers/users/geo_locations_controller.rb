ea2368aa22a66a540b34bb83be16a8c296495516
Niklas Mohrin <dev@niklasmohrin.de>
2022-01-13 14:39:14 +0100

    Use class instance var instead of class var and handle empty request parameters

--- app/controllers/users/geo_locations_controller.rb
+++ app/controllers/users/geo_locations_controller.rb
@@ -3,20 +3,29 @@ require "date"
 class Users::GeoLocationsController < ApplicationController
   # FIXME: this should be cleaned every now and then
   # Maps from user ids to (location, timestamp) tuples
-  @@locations = Concurrent::Hash.new
+  @locations = Concurrent::Hash.new
+
+  class << self
+    attr_accessor :locations
+  end
 
   def update_geo_location
     return head :unauthorized unless user_signed_in?
 
-    # TODO: should we validate that only exactly those are keys? maybe we
-    # lock ourself out of passing metadata / force us to clean out csrf
-    # tokens etc. beforehand?
-    location = params.require(:location)
-    unless RoutingHelper.valid_coordinates?(location)
-      return render(status: :bad_request,
-                    plain: "Invalid coordinates")
+    begin
+      # TODO: should we validate that only exactly those are keys? maybe we
+      # lock ourself out of passing metadata / force us to clean out csrf
+      # tokens etc. beforehand?
+      location = params.require(:location)
+    rescue ActionController::ParameterMissing
+      # TODO: find out why this isn't done automatically
+      return render status: :bad_request, plain: "Missing field `location`"
     end
 
-    @@locations[current_user.id] = [location, DateTime.now]
+    unless RoutingHelper.valid_coordinates?(location)
+      return render status: :bad_request, plain: "Invalid coordinates"
+    end
+
+    self.class.locations[current_user.id] = [location, DateTime.now]
   end
 end
