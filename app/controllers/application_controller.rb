aa5327c8b69b1ffb510f0216229a33bf7cefe197
d-schlereth <avi1992@hotmail.de>
2022-01-14 21:36:29 +0100

    Add language switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # Add additional allowed params for Users. Only include this code in devise controllers
   # This is a convenient way to customize devise controllers without creating a new ones
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :set_locale
 
   protected
 
@@ -13,5 +14,16 @@ class ApplicationController < ActionController::Base
       :account_update, keys: [:username, :first_name, :last_name, :phone_number, :rooms, :profile_picture,
                               { openingtimes_attributes: [:id, :day, :opens, :closes] }]
     )
+  end
+
+  private
+
+  def set_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
