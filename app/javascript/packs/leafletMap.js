92eb63b318f246fbf51f4cfd6093977b216a2c7b
rdunker <bm.rebekka@googlemail.com>
2022-01-17 16:36:10 +0100

    Remove console.log

--- app/javascript/packs/leafletMap.js
+++ app/javascript/packs/leafletMap.js
@@ -25,7 +25,6 @@ export async function setupMap() {
         getBuildings(),
         getBuildingMarkers(),
     ]);
-    console.log("test");
     setView(view);
     addTargetMarker();
     addPolygons(buildingPolygons);
