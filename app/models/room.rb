021e51b0cdd3a99a0e09a225cc49fc8416a21e0e
Benedikt Weber <benedikt.web23@gmail.com>
2022-01-20 23:21:40 +0100

    Combo box for room type

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,23 @@ class Room < ApplicationRecord
   has_and_belongs_to_many :people
   validates :name, presence: true
   validates :floor, presence: true, numericality: { only_integer: true }
+
+  def self.room_type_to_internal_mapping
+    {
+      'Lecture hall' => 'lecture-hall',
+      'Pool room' => 'pool-room',
+      'Seminar room' => 'seminar-room',
+      'Conference room' => 'conference-room',
+    }
+  end
+
+  def self.room_type_to_external_mapping
+    {
+      'lecture-hall' => 'Lecture hall',
+      'pool-room' => 'Pool room',
+      'seminar-room' => 'Seminar room',
+      'conference-room' => 'Conference room',
+    }
+  end
+
 end
