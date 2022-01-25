08bf172be06a2b205469d5d5eac9198e5d42ddae
Benedikt Weber <benedikt.web23@gmail.com>
2022-01-25 13:20:35 +0100

    Update user_authorization_spec.rb
    Co-authored-by: Youngbin Ko <50771294+YoungBinKo@users.noreply.github.com>

--- spec/features/users/user_authorization_spec.rb
+++ spec/features/users/user_authorization_spec.rb
@@ -2,7 +2,7 @@ require 'cancan/matchers'
 require 'rails_helper'
 
 describe 'User authorization', type: :feature do
-  it "is possible for an Admin can manage location, people and building" do
+  it "is possible for an Admin can manage location and building" do
     @user = create(:user, admin: true)
     ability = Ability.new(@user)
     expect(ability).to be_able_to(:manage, Location.new(users: [@user])) # location
@@ -10,7 +10,7 @@ describe 'User authorization', type: :feature do
     expect(ability).to be_able_to(:manage, Building.new(users: [@user])) # building
   end
 
-  it "is not possible for user without Admin permission to manage location, people and building" do
+  it "is not possible for user without Admin permission to manage location and building" do
     @user = create(:user, admin: false)
     @building = create :building
     @location = create :location
@@ -20,7 +20,7 @@ describe 'User authorization', type: :feature do
     expect(ability).not_to be_able_to(:manage, @building)
   end
 
-  it "is possible as user without Admin permission to update and delete an own location, people and building" do
+  it "is possible as user without Admin permission to update and delete an own location and building" do
     @user = create(:user, admin: false)
     ability = Ability.new(@user)
     expect(ability).to be_able_to(:manage, Location.new(users: [@user])) # location
