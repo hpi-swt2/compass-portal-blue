1eb85a6eb62c73827c84c250681e4ad6a173b2b7
codefactor-io <support@codefactor.io>
2022-01-25 18:31:16 +0000

    [CodeFactor] Apply fixes

--- db/migrate/20220124212152_create_join_table_building_owner.rb
+++ db/migrate/20220124212152_create_join_table_building_owner.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableBuildingOwner < ActiveRecord::Migration[6.1]
   def change
-    create_join_table :buildings, :users , table_name: 'building_owner' do |t|
+    create_join_table :buildings, :users, table_name: 'building_owner' do |t|
       # t.index [:building_id, :user_id]
       # t.index [:user_id, :building_id]
     end
