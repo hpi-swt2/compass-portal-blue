110937374323a9fcea8bac5d8c236775fbf4e5a0
Maximilian Franz <maximilian.franz@student.hpi.uni-potsdam.de>
2022-01-26 14:43:47 +0100

    Create event with start and end date as required

--- spec/views/events/edit.html.erb_spec.rb
+++ spec/views/events/edit.html.erb_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "events/edit", type: :view do
-  before(:each) do
-    @event = assign(:event, Event.create!(
-      name: "MyString",
-      description: "MyText",
-      recurring: "MyText"
-    ))
+  before do
+    now = Time.zone.now
+    @event = assign(:event,
+                    Event.create!(name: "MyString", d_start: now, d_end: now + 1.day, description: "MyText",
+                                  recurring: "MyText"))
   end
 
   it "renders the edit event form" do
@@ -15,14 +14,12 @@ RSpec.describe "events/edit", type: :view do
     assert_select "form[action=?][method=?]", event_path(@event), "post" do
 
       assert_select "input[name=?]", "event[name]"
-
       assert_select "textarea[name=?]", "event[description]"
-
       assert_select "textarea[name=?]", "event[recurring]"
-      
-      for i in 1..5 do
-        assert_select "select[name=?]", "event[d_start(#{i}i)]"
-        assert_select "select[name=?]", "event[d_end(#{i}i)]"
+
+      5.times do |i|
+        assert_select "select[name=?]", "event[d_start(#{i + 1}i)]"
+        assert_select "select[name=?]", "event[d_end(#{i + 1}i)]"
       end
 
       assert_select "select[name=?]", "event[room_id]"
