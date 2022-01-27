9c1acd0008005937adc4617c2bdea41792216df5
joh-dah <johanna@dahlkemper.de>
2022-01-27 17:42:31 +0100

    added coordinates to room model and show+edit page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_125451) do
+ActiveRecord::Schema.define(version: 2022_01_27_162451) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_125451) do
     t.float "location_longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_125451) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "location_latitude"
     t.float "location_longitude"
+    t.integer "user_id"
   end
 
   create_table "openingtimes", force: :cascade do |t|
@@ -90,6 +92,8 @@ ActiveRecord::Schema.define(version: 2021_12_10_125451) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "building_id"
     t.integer "user_id"
+    t.float "location_latitude"
+    t.float "location_longitude"
     t.index ["building_id"], name: "index_rooms_on_building_id"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
@@ -106,6 +110,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_125451) do
     t.string "uid"
     t.string "username"
     t.integer "person_id"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["person_id"], name: "index_users_on_person_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
