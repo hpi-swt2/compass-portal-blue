a0d2a26eb54941061c7977261b9a2066eba5e630
Stroyxx <rk81ogel@gmail.com>
2022-01-27 15:36:11 +0100

    Remove new-lines

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     association :room
     name { "BA Mathematik III Übung" }
     description { "Teaching mathematics" }
-
     d_start { "2021-10-25 13:15:00" }
     d_end { "2021-10-25 14:45:00" }
 
@@ -24,6 +23,5 @@ FactoryBot.define do
     end
 
     recurring { IceCube::Rule.weekly.day(:monday).to_yaml }
-
   end
 end
