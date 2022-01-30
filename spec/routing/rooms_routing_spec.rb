5ecf664599c2228e1bc258e1b33b443f451faf94
Flo-Mueller <florian.mueller141@web.de>
2022-01-30 15:21:34 +0100

    Fixed Tests and UI

--- spec/routing/rooms_routing_spec.rb
+++ spec/routing/rooms_routing_spec.rb
@@ -1,6 +1,23 @@
 require "rails_helper"
 
 RSpec.describe RoomsController, type: :routing do
+  before do
+    @building = create :building
+    @people = [(create :person)]
+    @room = assign(:room, Room.create!(
+                            name: "Seminarraum 1",
+                            floor: 0,
+                            room_type: "seminar-room",
+                            people: @people,
+                            building: @building
+                          ))
+    @events = [Event.create!(name: "Test Event", description: "CG", d_start: Time.zone.now,
+                             d_end: Time.zone.now, recurring: "", room_id: @room.id)]
+    @date = Time.zone.now
+    @month = Date::MONTHNAMES[Time.zone.today.month]
+    @year = Time.zone.now.year
+  end
+  
   describe "routing" do
     it "routes to #index" do
       expect(get: "/rooms").to route_to("rooms#index")
