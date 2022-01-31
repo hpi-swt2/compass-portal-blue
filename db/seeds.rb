d2e58fb1dbd4da85536e25c23d40edc2f218b2ae
codefactor-io <support@codefactor.io>
2022-01-31 16:36:42 +0000

    [CodeFactor] Apply fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ require 'ice_cube'
 hsg = Building.create!(name: "Hörsaalgebäude", location_latitude: 0.5321, location_longitude: 0.8763)
 hptg = Building.create!(name: "Hauptgebäude", location_latitude: 0.1674, location_longitude: 0.2345)
 hs1 = Room.create!(name: "Hörsaal1", floor: 0, room_type: "lecture-hall", building: hsg, location_latitude: 0.5322,
-    location_longitude: 0.8764)
+                   location_longitude: 0.8764)
 hs2 = Room.create!(name: "Hörsaal2", floor: 0, room_type: "lecture-hall", building: hsg, location_latitude: 0.5522,
-    location_longitude: 0.9764)
+                   location_longitude: 0.9764)
 h257 = Room.create!(name: "H.257", floor: 2, room_type: "seminar-room", building: hptg, location_latitude: 0.5122,
-    location_longitude: 0.8754)
+                    location_longitude: 0.8754)
 Event.create!(name: "Mathe 1", description: "Mathe 1 Vorlesung bei Meinel", d_start: "2022-01-23 10:58:13",
-    d_end: "2022-01-23 11:58:13", recurring: "", room: hs1)
+              d_end: "2022-01-23 11:58:13", recurring: "", room: hs1)
 Event.create!(name: "Betriebssysteme", description: "BS", d_start: "2022-01-03 08:00:13", d_end: "2022-01-03 9:58:13",
-    recurring: IceCube::Rule.weekly.day(:monday).to_yaml, room: hs2)
+              recurring: IceCube::Rule.weekly.day(:monday).to_yaml, room: hs2)
 Event.create!(name: "CG", description: "CG", d_start: "2022-01-21 10:58:13", d_end: "2022-01-21 11:58:13",
-    recurring: "", room: h257)
+              recurring: "", room: h257)
