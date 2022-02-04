e58d388e6c521e02939dc124616101ecc9a79086
piabeeck <hellopia@gmx.de>
2022-02-04 12:09:59 +0100

    Added model test.

--- spec/models/favourite_room_spec.rb
+++ spec/models/favourite_room_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe FavouriteRoom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has user_id and room_id" do
+    favourite_room = FactoryBot.create :favourite_room
+    room = FactoryBot.create :room
+    user = FactoryBot.create :user
+
+    expect(favourite_room.room.name).to eq(room.name)
+    expect(favourite_room.user.username).to eq(user.username)
+  end
+
+  it "has a room" do
+    favourite_room = described_class.reflect_on_association(:room)
+    expect(favourite_room.macro).to eq :belongs_to
+  end
+
+  it "has a user" do
+    favourite_room = described_class.reflect_on_association(:user)
+    expect(favourite_room.macro).to eq :belongs_to
+  end
+
 end
