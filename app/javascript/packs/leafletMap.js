734bcdadfb889bb591bd34f3331dc24c43625c57
postmartem <67064988+postmartem@users.noreply.github.com>
2022-02-08 17:18:45 +0100

    Update app/javascript/packs/leafletMap.js
    Co-authored-by: Niklas Mohrin <dev@niklasmohrin.de>

--- app/javascript/packs/leafletMap.js
+++ app/javascript/packs/leafletMap.js
@@ -92,7 +92,7 @@ function pinOnClick(pinNumber, position){
     .openOn(map);
   document.getElementById("deletepin").addEventListener("click", function(e) {
     removePinAndPopup(pinNumber, popup);
-    }, false);
+  }, false);
 }
 
 function removePinAndPopup(pinNumber, popup){
