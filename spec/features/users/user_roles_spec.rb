9087b28253eaa84ae1f7b33f178173a1e5c28dd9
Tamino Bauknecht <dev@tb6.eu>
2022-02-08 19:52:12 +0100

    Crappy Shitty Rotten Foken

--- spec/features/users/user_roles_spec.rb
+++ spec/features/users/user_roles_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe "User roles page", type: :feature do
     admin_user = create(:user, admin: true)
     sign_in admin_user
     visit users_roles_path
-    expect(@user.admin).to be(false)
-    page.find("input#user-admin-#{@user.id}").click
-    expect(@user.admin).to be(true)
+    expect(page.find("input#user-admin-#{@user.id}")).not_to be_checked
+    page.check("user-admin-#{@user.id}")
+    visit users_roles_path
+    expect(page.find("input#user-admin-#{@user.id}")).to be_checked
   end
-
 end
