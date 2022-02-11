eb95a954e5c06d543c0d3c30d5022cd4858d6d48
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2022-02-11 11:42:15 +0100

    fix tests(again) +1

--- spec/routing/errors_routing_spec.rb
+++ spec/routing/errors_routing_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe ErrorController, type: :routing do
 
     it "redirects from rooms/new to error/show" do
       get "/rooms/new"
-      expect(response).to redirect_to("/error/show?local=en")
+      expect(response).to redirect_to("/error/show?locale=en")
     end
 
     it "redirects from people/new to error/show" do
       get "/people/new"
-      expect(response).to redirect_to("/error/show?local=en")
+      expect(response).to redirect_to("/error/show?locale=en")
     end
 
     it "redirects from locations/new to error/show" do
       get "/locations/new"
-      expect(response).to redirect_to("/error/show?local=en")
+      expect(response).to redirect_to("/error/show?locale=en")
     end
 
     it "redirects from buildings/new to error/show" do
       get "/buildings/new"
-      expect(response).to redirect_to("/error/show?local=en")
+      expect(response).to redirect_to("/error/show?locale=en")
     end
   end
 end
