1509c0c7c5444dd61bfe7b8c31f7b786145b84ad
fhradilak <82701668+fhradilak@users.noreply.github.com>
2022-02-11 14:11:57 +0100

    updarte error test

--- spec/features/errors/show_error_details_spec.rb
+++ spec/features/errors/show_error_details_spec.rb
@@ -4,7 +4,7 @@ describe "Error Show Page", type: :feature do
 
   it "displays an error when not logged in and visiting a <>/new route" do
     visit new_person_path
-    expect(page).to have_text "You are not authorized to access this page. Please log in or create an user to edit."
+    expect(page).to have_text "You are not authorized to access this page. Please log in or sign up to edit."
 
   end
 
