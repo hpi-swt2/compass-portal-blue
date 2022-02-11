7cf9f2eabae678d3498c3583487432dd7fd74c56
JulianEgbert <Julian.Egbert@student.hpi.de>
2022-02-11 21:00:01 +0100

    remove code comment regarding assets

--- app/assets/constants/indoor_graphs.rb
+++ app/assets/constants/indoor_graphs.rb
@@ -7,8 +7,7 @@ module IndoorGraph
 
   # rubocop:disable Metrics/AbcSize
   def self.load_building(building)
-    # file_name = ActionController::Base.helpers.asset_path(building << ".json")
-    file_name = "./app/assets/graphs/" << building << ".json" # FIXME: Adapt assets path
+    file_name = "./app/assets/graphs/" << building << ".json"
     indoor_graphs[building] = JSON.parse(File.read(file_name))
     entry_nodes[building] = indoor_graphs[building].select { |_key, node| node['entry'] }.keys
     nodes.merge!(building => indoor_graphs[building].keys)
