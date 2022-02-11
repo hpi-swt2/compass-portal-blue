f078eed18c91ab465c65447bc697c1bc512679a3
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2022-02-11 13:33:46 +0100

    clean up error_controller.rb

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -1,7 +1,6 @@
 class ErrorController < ApplicationController
   def show
     exception = params[:exception]
-    logger.debug "BAUM: #{exception} .... #{exception.inspect}"
     @message = exception.inspect
   end
 end
