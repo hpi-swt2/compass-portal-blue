99b66f09aef26af25f8b734f56af45535d75f2d1
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2022-02-11 11:22:17 +0100

    fix tests add error show.html

--- spec/routing/errors_routing_spec.rb
+++ spec/routing/errors_routing_spec.rb
@@ -1,25 +1,26 @@
 require "rails_helper"
 
-RSpec.describe BuildingsController, type: :routing do
+RSpec.describe ErrorController, type: :routing do
   describe "redirecting", type: :request do
+    @route_add_on = "?local=en"
     it "redirects from rooms/new to error/show" do
       get "/rooms/new"
-      expect(response).to redirect_to("/error/show")
+      expect(response).to redirect_to("/error/show#{@route_add_on}")
     end
 
     it "redirects from people/new to error/show" do
       get "/people/new"
-      expect(response).to redirect_to("/error/show")
+      expect(response).to redirect_to("/error/show#{@route_add_on}")
     end
 
     it "redirects from locations/new to error/show" do
       get "/locations/new"
-      expect(response).to redirect_to("/error/show")
+      expect(response).to redirect_to("/error/show#{@route_add_on}")
     end
 
     it "redirects from buildings/new to error/show" do
       get "/buildings/new"
-      expect(response).to redirect_to("/error/show")
+      expect(response).to redirect_to("/error/show#{@route_add_on}")
     end
   end
 end
