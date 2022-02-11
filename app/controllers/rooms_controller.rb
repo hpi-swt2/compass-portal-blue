89c85cdcfa5164c744f4aeb3ea2c54027e3dd626
Benedikt Weber <benedikt.web23@gmail.com>
2022-02-11 13:28:39 +0100

    Integrate into the single page application

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -72,37 +72,14 @@ class RoomsController < ApplicationController
 
   def favourite
     @room = Room.find(params[:id])
-    type = params[:type]
-    url = params[:url]
+    favourite = params[:favourite]
 
-    if type == "favourite"
-      current_user.favourites << @room
-      if url == "rooms"
-        redirect_to rooms_url, notice: "You favourited #{@room.name}"
-      elsif url == "favouriteroomlist"
-        redirect_to favouriteroomlist_url, notice: "You favourited #{@room.name}"
-      elsif url == "show"
-        redirect_to @room, notice: "You favourited #{@room.name}"
-      else
-        redirect_to rooms_url, notice: "You favourited #{@room.name}"
+    if favourite
+      if not current_user.favourites.include? @room
+        current_user.favourites << @room
       end
-
-    elsif type == "unfavourite"
-      current_user.favourites.delete(@room)
-
-      if url == "rooms"
-        redirect_to rooms_url, notice: "You unfavourited #{@room.name}"
-      elsif url == "favouriteroomlist"
-        redirect_to favouriteroomlist_url, notice: "You unfavourited #{@room.name}"
-      elsif url == "show"
-        redirect_to @room, notice: "You unfavourited #{@room.name}"
-      else
-        redirect_to rooms_url, notice: "You unfavourited #{@room.name}"
-      end
-
     else
-      # Type missing, nothing happens
-      redirect_to rooms_url, notice: "Nothing happened."
+      current_user.favourites.delete(@room)
     end
   end
 
