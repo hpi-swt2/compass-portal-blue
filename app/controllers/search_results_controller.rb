167a8e4a85a32df3195efaa67e269cd627ef6cf3
An <hoangan.nguyen@student.hpi.de>
2022-02-15 00:04:35 +0100

    linter please

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -91,11 +91,11 @@ class SearchResultsController < ApplicationController
 
   def distance(loc1, loc2)
     rad_per_deg = Math::PI / 180 # PI / 180
-    dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg # Delta, converted to rad
-    dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg
     lat1_rad = loc1.map { |i| i * rad_per_deg }.first
     lat2_rad = loc2.map { |i| i * rad_per_deg }.first
-    a = (Math.sin(dlat_rad / 2)**2) + (Math.cos(lat1_rad) * Math.cos(lat2_rad) * (Math.sin(dlon_rad / 2)**2))
+    a = (Math.sin(((loc2[0] - loc1[0]) * rad_per_deg) / 2)**2) +
+    (Math.cos(lat1_rad) * Math.cos(lat2_rad) *
+    (Math.sin((dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg) / 2)**2))
     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
     6_371_000 * c # Delta in meters
   end
@@ -112,10 +112,8 @@ class SearchResultsController < ApplicationController
            (current_user.last_known_location_with_timestamp[1] >= 1.minute.ago)
       return
     end
-
     current_position = current_user.last_known_location_with_timestamp[0].split(',').map(&:to_f)
-    @search_results = @search_results.sort_by { |result|
-      distance(current_position, [result.location_latitude, result.location_longitude])
-    }
+    @search_results = 
+    @search_results.sort_by { |r| distance(current_position, [r.location_latitude, r.location_longitude])}
   end
 end
