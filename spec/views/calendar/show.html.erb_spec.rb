21564fe1f63ea716bf39d1b7712a3b207485d2c8
Stroyxx <rk81ogel@gmail.com>
2022-02-14 15:16:31 +0100

    add more tests

--- spec/views/calendar/show.html.erb_spec.rb
+++ spec/views/calendar/show.html.erb_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "calendar/show", type: :view do
                              end_time: 1.hour.from_now, recurring: "", room: (create :room)),
                Event.create!(name: "Another Test Event", description: "PT", start_time: 1.hour.from_now,
                              end_time: 2.hours.from_now, recurring: "", room: (create :room))]
-                            
+
     @highlighted_event_id = 1
     @date = Time.zone.now
     @month = Time.zone.today.month
@@ -29,10 +29,10 @@ RSpec.describe "calendar/show", type: :view do
     expect(rendered).to have_text(@events.second.start_hour_minute)
   end
 
-  it "should have events highlighted correctly" do
+  it "has events highlighted correctly" do
     render
-    expect(rendered).to have_css(".highlighted_event", :text => @events.first.name)
-    expect(rendered).not_to have_css(".highlighted_event", :text => @events.second.name)
+    expect(rendered).to have_css(".highlighted_event", text: @events.first.name)
+    expect(rendered).not_to have_css(".highlighted_event", text: @events.second.name)
   end
 
 end
