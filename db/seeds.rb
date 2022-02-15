10d275fd49916e93dcc7a048857789db1108e76d
codefactor-io <support@codefactor.io>
2022-02-15 18:09:48 +0000

    [CodeFactor] Apply fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,37 +4,41 @@ require 'ice_cube'
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 def avg(coordinates)
-    x = coordinates.reduce(0) { | acc, current | acc + current[0] } / coordinates.length
-    y = coordinates.reduce(0) { | acc, current | acc + current[1] } / coordinates.length
-    return [x, y]
+  x = coordinates.reduce(0) { |acc, current| acc + current[0] } / coordinates.length
+  y = coordinates.reduce(0) { |acc, current| acc + current[1] } / coordinates.length
+  [x, y]
 end
 
 buildings_file = File.read("app/assets/geojsons/buildings.geojson")
 buildings_json = JSON.parse(buildings_file)
 
-buildings_json["features"].each do | building |
-    next if not building["properties"]["name"] or not  building["properties"]["letter"]
-    long, lat = avg(building["geometry"]["coordinates"][0])
-    building_obj = Building.create!(name: building["properties"]["name"], location_latitude: lat, location_longitude: long)
+buildings_json["features"].each do |building|
+  next if !(building["properties"]["name"]) || !(building["properties"]["letter"])
 
-    Dir.foreach('app/assets/geojsons') do |filename|
-        next if filename == '.' or filename == '..' or !filename.include?("#{building["properties"]["letter"]}")
-        file = File.read("app/assets/geojsons/#{filename}")
-        json = JSON.parse(file)
-        json["features"].each do | feature |
-            if feature["properties"]["indoor"] === 'room' && feature["properties"]["name-en"] && feature["properties"]["building"] && building["properties"]["letter"] == feature["properties"]["building"]
-                long, lat = avg(feature["geometry"]["coordinates"][0])
-                Room.create!(
-                    name: feature["properties"]["name-en"],
-                    floor: Integer(feature["properties"]["level_name"]),
-                    room_type: feature["properties"]["type"],
-                    building: building_obj,
-                    location_latitude: lat,
-                    location_longitude: long
-                )
-            end
+  long, lat = avg(building["geometry"]["coordinates"][0])
+  building_obj = Building.create!(name: building["properties"]["name"], location_latitude: lat, location_longitude: long)
+
+  Dir.foreach('app/assets/geojsons') do |filename|
+    next if (filename == '.') || (filename == '..') || filename.exclude?((building['properties']['letter']).to_s)
+
+    file = File.read("app/assets/geojsons/#{filename}")
+    json = JSON.parse(file)
+    json["features"].each do |feature|
+      unless feature["properties"]["indoor"] === 'room' && feature["properties"]["name-en"] && feature["properties"]["building"] && building["properties"]["letter"] == feature["properties"]["building"]
+        next
         end
+
+      long, lat = avg(feature["geometry"]["coordinates"][0])
+      Room.create!(
+        name: feature["properties"]["name-en"],
+        floor: Integer(feature["properties"]["level_name"]),
+        room_type: feature["properties"]["type"],
+        building: building_obj,
+        location_latitude: lat,
+        location_longitude: long
+      )
     end
+  end
 end
 
 # Event.create!(name: "Mathe 1", description: "Mathe 1 Vorlesung bei Meinel", start_time: "2022-01-23 10:58:13",
