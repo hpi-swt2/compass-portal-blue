eebe3cebb850d0f3d64435d3ceeda71f9b851ee2
TheGrayStone <nilslissner@gmx.de>
2022-02-17 13:50:50 +0100

    Add import script and add (german) names to buildings.geojson (#248)
    * Add import script and add (german) names to buildings.geojson
    
    * Add updated geojsons ABC and HS, adapt seed
    
    * Adapted G-1 geojson
    
    * Updates remaining parts of building G
    
    * Update villa geojsons
    
    * Add H_3 geojson
    
    * Add Building H and seed DB
    
    * Fixes H_0 and H_1 geojson
    
    * Replace Linestring with Polygon in H_3.geojson
    
    * [CodeFactor] Apply fixes
    
    * Formatting and building B and C correction
    
    * linting
    
    * Fix line too long
    
    * make letter_coordinate a point
    
    * Fix leaflet label name
    
    * Use room coordinates instead of building in show room
    
    * Update seeds.rb
    
    Co-authored-by: oleschl <49571011+oleschl@users.noreply.github.com>
    Co-authored-by: Julian Schmidt <julian.r.schmidt.js@gmail.com>
    Co-authored-by: d-schlereth <avi1992@hotmail.de>
    Co-authored-by: codefactor-io <support@codefactor.io>

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,50 @@ require 'ice_cube'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-hsg = Building.create!(name: "Hörsaalgebäude", location_latitude: 52.394084, location_longitude: 13.132397)
-hptg = Building.create!(name: "Hauptgebäude", location_latitude: 52.393889, location_longitude: 13.133515)
-hs1 = Room.create!(name: "Hörsaal1", floor: 0, room_type: "lecture-hall", building: hsg, location_latitude: 52.394058,
-                   location_longitude: 13.132626)
-hs2 = Room.create!(name: "Hörsaal2", floor: 0, room_type: "lecture-hall", building: hsg, location_latitude: 52.394163,
-                   location_longitude: 13.132283)
-h257 = Room.create!(name: "H.257", floor: 2, room_type: "seminar-room", building: hptg, location_latitude: 52.394042,
-                    location_longitude: 13.133324)
-Event.create!(name: "Mathe 1", description: "Mathe 1 Vorlesung bei Meinel", start_time: "2022-01-23 10:58:13",
-              end_time: "2022-01-23 11:58:13", recurring: "", room: hs1)
-Event.create!(name: "Betriebssysteme", description: "BS", start_time: "2022-01-03 08:00:13",
-              end_time: "2022-01-03 9:58:13", recurring: IceCube::Rule.weekly.day(:monday).to_yaml, room: hs2)
-Event.create!(name: "CG", description: "CG", start_time: "2022-01-21 10:58:13", end_time: "2022-01-21 11:58:13",
-              recurring: "", room: h257)
+
+def avg(coordinates)
+  x = coordinates.reduce(0) { |acc, current| acc + current[0] } / coordinates.length
+  y = coordinates.reduce(0) { |acc, current| acc + current[1] } / coordinates.length
+  [x, y]
+end
+
+buildings_file = File.read("app/assets/geojsons/buildings.geojson")
+buildings_json = JSON.parse(buildings_file)
+
+buildings_json["features"].each do |building|
+  next if !(building["properties"]["name"]) || !(building["properties"]["letter"])
+
+  long, lat = avg(building["geometry"]["coordinates"][0])
+  building_obj = Building.create!(name: building["properties"]["name"], location_latitude: lat,
+                                  location_longitude: long)
+
+  Dir.foreach('app/assets/geojsons') do |filename|
+    next if (filename == '.') || (filename == '..') || filename.exclude?((building['properties']['letter']).to_s)
+
+    file = File.read("app/assets/geojsons/#{filename}")
+    json = JSON.parse(file)
+    json["features"].each do |feature|
+      is_feature_in_building = building["properties"]["letter"] == feature["properties"]["building"]
+      unless feature["properties"]["indoor"] == 'room' && feature["properties"]["name-en"] && is_feature_in_building
+        next
+      end
+
+      long, lat = avg(feature["geometry"]["coordinates"][0])
+      Room.create!(
+        name: feature["properties"]["name-en"],
+        floor: Integer(feature["properties"]["level_name"]),
+        room_type: feature["properties"]["type"],
+        building: building_obj,
+        location_latitude: lat,
+        location_longitude: long
+      )
+    end
+  end
+end
+
+# Event.create!(name: "Mathe 1", description: "Mathe 1 Vorlesung bei Meinel", start_time: "2022-01-23 10:58:13",
+#     end_time: "2022-01-23 11:58:13", recurring: "", room: hs1)
+# Event.create!(name: "Betriebssysteme", description: "BS", start_time: "2022-01-03 08:00:13",
+#     end_time: "2022-01-03 9:58:13", recurring: IceCube::Rule.weekly.day(:monday).to_yaml, room: hs2)
+# Event.create!(name: "CG", description: "CG", start_time: "2022-01-21 10:58:13", end_time: "2022-01-21 11:58:13",
+#     recurring: "", room: h257)
